d0b6b84d0c761de8de1028d0e4fa6b6220500142
andresfcano <andrescano30@gmail.com>
2025-11-15 00:19:01 -0500

    Mobile fixes and email copy

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,11 +3,30 @@ import Sidebar from '../Sidebar/'
 import './index.scss'
 
 const Layout = () => {
+  const handleSkipToContent = () => {
+    const main = document.getElementById('main')
+    if (main) {
+      // Make sure it's focusable, then focus & scroll to it
+      main.setAttribute('tabindex', '-1')
+      main.focus()
+      main.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    }
+  }
   return (
     <div className="App">
+      <button
+        type="button"
+        className="skip-link"
+        onClick={handleSkipToContent}
+      >
+        Skip to content
+      </button>
+      <a href="#main" className="skip-link">Skip to content</a>
       <Sidebar />
       <div className="page">
-        <Outlet />
+        <main id="main">
+          <Outlet />
+        </main>
       </div>
     </div>
   )
